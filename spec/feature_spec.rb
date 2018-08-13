ed2c1eb844b5c464b3d8b000907d7e812de64ed1
Elishka Flint <elishkaf@gmail.com>
2018-08-13 13:34:47 +0100

    TEST Add feature test for withdrawal

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,16 +2,30 @@ require 'statement'
 
 describe Statement do
 
+  before(:each) do
+    account = Account.new
+    date = Time.now.strftime("%d/%m/%Y")
+  end
+
   # As a user,
   # So that I can move money into my account,
   # I want to be able to make a deposit.
 
   describe '#deposit' do
-    it 'adds a new transation to the statement' do
-      statement = Statement.new
-      account = Account.new
-      date = Time.now.strftime("%d/%m/%Y")
+    it 'adds a new credit transaction to the statement' do
       account.deposit(10, date)
+      msg = "date || credit || debit || balance\n#{date} || || 10.00 || 10.00"
+      expect(account.summary).to eq msg
+    end
+  end
+
+  # As a user,
+  # So that I can spend the money in my account,
+  # I want to be able to make a withdrawal.
+
+  describe '#withdrawal' do
+    it 'adds a new debit transation to the statement' do
+      account.withdraw(10, date)
       msg = "date || credit || debit || balance\n#{date} || || 10.00 || 10.00"
       expect(statement.summary).to eq msg
     end
