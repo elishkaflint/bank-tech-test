85352cf9d2e02739d017fa88902e39adf659a015
Elishka Flint <elishkaf@gmail.com>
2018-08-14 09:51:39 +0100

    TEST Add test for balance functionality being extracted from account class

---
+++ spec/balance_spec.rb
@@ -0,0 +1,27 @@
+require 'balance'
+
+describe Balance do
+
+  let(subject) { Balance.new }
+
+  describe '#initialize' do
+    it 'starts with a default balance' do
+      expect(subject.balance).to eq Balance::DEFAULT_BALANCE
+    end
+  end
+
+  describe '#increase' do
+    it 'increases the balance' do
+      subject.increase(5)
+      expect(subject.balance).to eq 5
+    end
+  end
+
+  describe '#decrease' do
+    it 'decreases the balance' do
+      subject.decrease(5)
+      expect(subject.balance).to eq -5
+    end
+  end
+
+end
